e0476d72d6b50ffb92336ca5f687ee34120d2ffe
Venkatesh Thimmaiah <venkatesh131755@gmail.com>
2025-09-15 11:51:35 +0530

    Change: adjusted home page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,7 +3,7 @@ export default function Body() {
     <div className=" p-4 mt-[36rem] lg:mt-[20rem]">
       <h1 className="text-block text-3xl font-bold">
         Providing Advertisers With High Impact Access To{" "}
-        <spann>Urban India</spann>
+        <span>Urban India</span>
       </h1>
       <img
         src="https://adonmo.com/wp-content/uploads/2025/06/Map27-768x854.png"
